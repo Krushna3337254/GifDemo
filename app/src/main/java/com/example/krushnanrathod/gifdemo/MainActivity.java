0f2413b26a4e34e697cf2e633d61d1b2d1264a14
Krushna N. Rathod <knr3337254@gmail.com>
2019-05-31 12:41:59 +0530

    GIF DEMO

---
+++ app/src/main/java/com/example/krushnanrathod/gifdemo/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.krushnanrathod.gifdemo;
+
+import android.support.constraint.ConstraintLayout;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    ConstraintLayout layout;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+
+    }
+}
